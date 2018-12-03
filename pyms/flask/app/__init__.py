74afab319c033f0638d739f78c36f9ccaab7e7ac
Alberto Vara <a.vara.1986@gmail.com>
2018-12-03 21:25:24 +0100

    Refactor initializations of the app

--- pyms/flask/app/__init__.py
+++ pyms/flask/app/__init__.py
@@ -22,6 +22,20 @@ class Microservice:
     def init_libs(self):
         return self.application
 
+    def init_tracer(self):
+        self.application.tracer = FlaskTracer(init_jaeger_tracer(), True, self.application)
+
+    def init_logger(self):
+        formatter = CustomJsonFormatter('(timestamp) (level) (name) (module) (funcName) (lineno) (message)')
+        formatter.add_service_name(self.application.config["APP_NAME"])
+        if getattr(self.application, "tracer", False):
+            formatter.add_trace_span(self.application.tracer)
+        log_handler = logging.StreamHandler()
+        log_handler.setFormatter(formatter)
+        self.application.logger.addHandler(log_handler)
+        self.application.logger.propagate = False
+        self.application.logger.setLevel(logging.INFO)
+
     def create_app(self):
         """Initialize the Flask app, register blueprints and initialize
         all libraries like Swagger, database,
@@ -47,18 +61,10 @@ class Microservice:
         self.init_libs()
         self.add_error_handlers()
 
-        # Inject Modules
-        formatter = CustomJsonFormatter('(timestamp) (level) (name) (module) (funcName) (lineno) (message)')
         if not self.application.config["TESTING"]:
-            log_handler = logging.StreamHandler()
+            self.init_tracer()
 
-            self.application.tracer = FlaskTracer(init_jaeger_tracer(), True, self.application)
-            formatter.add_service_name(self.application.config["APP_NAME"])
-            formatter.add_trace_span(self.application.tracer)
-            log_handler.setFormatter(formatter)
-            self.application.logger.addHandler(log_handler)
-            self.application.logger.propagate = False
-            self.application.logger.setLevel(logging.INFO)
+        self.init_logger()
 
         return self.application
 
@@ -74,5 +80,4 @@ class Microservice:
         :param code_or_exception: HTTP error code or exception
         :param handler: callback for error handler
         """
-
         self.application._connexion_app.add_error_handler(code_or_exception, handler)
