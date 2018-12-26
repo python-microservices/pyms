cdeb753e67a100193baeb945f254765e999d3623
Javier Luna molina <javierlunamolina@gmail.com>
2018-12-26 21:16:28 +0100

    Update setup.py: Change development status from mature to alpha, add Flask framework, add Python 3.7

--- setup.py
+++ setup.py
@@ -14,9 +14,23 @@ if os.path.exists('README.md'):
 else:
     long_description = ''
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-with open('requirements.txt') as f:
-    required = [lib for lib in f.read().splitlines() if not lib.startswith("-e")]
+install_requires = [
+    'flask<1.1,>=1.0.2',
+    'anyconfig<1.0.0,>=0.9.7',
+    'python-json-logger<0.2.0,>=0.1.10',
+    'connexion[swagger-ui]<2.2.0,>=2.1.0',
+    'Flask-OpenTracing<0.3.0,>=0.2.0',
+    'jaeger-client<3.13.0,>=3.12.0'
+]
+
+tests_require = [
+    'coverage<4.6.0,>=4.5.2',
+    'mock<2.1.0,>=2.0.0',
+    'nose<1.4.0,>=1.3.7',
+    'pylint<2.3.0,>=2.2.2',
+    'tox<3.7.0>=3.6.0',
+    'requests_mock<1.6.0,>=1.5.2'
+]
 
 setup(
     name="py-ms",
@@ -26,18 +40,21 @@ setup(
     description="",
     long_description=long_description,
     classifiers=[
-        'Development Status :: 6 - Mature',
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    license="Proprietary",
+    license="License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     platforms=["any"],
     keywords="",
     url='',
     test_suite='nose.collector',
     packages=find_packages(),
-    install_requires=required,
+    install_requires=install_requires,
+    tests_require=tests_require,
     include_package_data=True,
     zip_safe=True,
 )
