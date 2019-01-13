60b380e8148f6d949d7ea857659891c74a2c5669
Alberto Vara <a.vara.1986@gmail.com>
2019-01-13 21:39:01 +0100

    Refactor setup

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import codecs
 import os
 
+import json
 from setuptools import setup, find_packages
 
 author = __import__('pyms').__author__
@@ -14,23 +15,21 @@ if os.path.exists('README.md'):
 else:
     long_description = ''
 
-install_requires = [
-    'flask<1.1,>=1.0.2',
-    'anyconfig<1.0.0,>=0.9.7',
-    'python-json-logger<0.2.0,>=0.1.10',
-    'connexion[swagger-ui]<2.2.0,>=2.1.0',
-    'Flask-OpenTracing<0.3.0,>=0.2.0',
-    'jaeger-client<3.13.0,>=3.12.0'
-]
 
-tests_require = [
-    'coverage<4.6.0,>=4.5.2',
-    'mock<2.1.0,>=2.0.0',
-    'nose<1.4.0,>=1.3.7',
-    'pylint<2.3.0,>=2.2.2',
-    'tox<3.7.0>=3.6.0',
-    'requests_mock<1.6.0,>=1.5.2'
-]
+
+install_requires = []
+tests_require = []
+if os.path.exists('Pipfile.lock'):
+    with open('Pipfile.lock') as fd:
+        lock_data = json.load(fd)
+        install_requires = [
+            package_name + package_data['version']
+            for package_name, package_data in lock_data['default'].items()
+        ]
+        tests_require = [
+            package_name + package_data['version']
+            for package_name, package_data in lock_data['develop'].items()
+        ]
 
 setup(
     name="py-ms",
