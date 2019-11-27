5b36a906c7927fa3e442b63515c9773c5822eb5a
Alex Perez-Pujol <alexperez@paradigmadigital.com>
2019-11-27 11:00:11 +0100

    Deacoplate class of logger function

--- pyms/flask/metrics/logger.py
+++ pyms/flask/metrics/logger.py
@@ -11,12 +11,19 @@ log_entries = prometheus_client.Counter(
 )
 
 
+class ExportingLogHandler(logging.Handler):
+    """A LogHandler that exports logging metrics for Prometheus.io."""
+
+    # def __init__(self, app_name, *args, **kwargs):
+    def __init__(self, app_name):
+        super(ExportingLogHandler, self).__init__()
+        self.app_name = app_name
+
+    def emit(self, record):
+        log_entries.labels(self.app_name, record.levelname).inc()
+
+
 def logger_metrics(app_name, logger):
-    class ExportingLogHandler(logging.Handler):
-        """A LogHandler that exports logging metrics for Prometheus.io."""
 
-        def emit(self, record):
-            log_entries.labels(app_name, record.levelname).inc()
-
-    logger.addHandler(ExportingLogHandler())
+    logger.addHandler(ExportingLogHandler(app_name))
     return logger
