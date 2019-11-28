b5f235cd4bd0b6b777a5974d0814386a02eb17af
Alex Perez-Pujol <alexperez@paradigmadigital.com>
2019-11-28 20:34:19 +0100

    Test metrics endpoint

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,7 +5,6 @@ from flask import Flask
 from pyms.flask.metrics import metrics_blueprint, monitor
 from prometheus_client import generate_latest
 
-
 class TestMetricsFlask(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
@@ -14,12 +13,18 @@ class TestMetricsFlask(unittest.TestCase):
 
     def test_metrics_latency(self):
         monitor(self.app)
-        generated_latency = b'flask_request_latency_seconds_bucket{endpoint="/",le="0.005",method="GET"}'
-        assert generated_latency in generate_latest()
+        self.client.get("/")
+        self.client.get("/metrics")
+        generated_latency_root = b'flask_request_latency_seconds_bucket{endpoint="/",le="0.005",method="GET"}'
+        generated_latency_metrics = b'flask_request_latency_seconds_bucket{endpoint="/metrics",le="0.005",method="GET"}'
+        assert generated_latency_root in generate_latest()
+        assert generated_latency_metrics in generate_latest()
 
     def test_metrics_count(self):
         monitor(self.app)
-        generated_count = (
-            b'flask_request_count_total{endpoint="/",http_status="404",method="GET"}'
-        )
-        assert generated_count in generate_latest()
+        self.client.get("/")
+        self.client.get("/metrics")
+        generated_count_root = b'flask_request_count_total{endpoint="/",http_status="404",method="GET"}'
+        generated_count_metrics = b'flask_request_count_total{endpoint="/metrics",http_status="200",method="GET"}'
+        assert generated_count_root in generate_latest()
+        assert generated_count_metrics in generate_latest()
