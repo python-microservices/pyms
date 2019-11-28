656b498772babc85293caf4a677a5c6990548a42
Miguel Garcia Rubin <mgrubin@paradigmadigital.com>
2019-11-28 13:51:20 +0100

    Update Connexion Framework, parametrize memoizing

--- pyms/config/conf.py
+++ pyms/config/conf.py
@@ -7,8 +7,9 @@ __service_configs = {}
 
 def get_conf(*args, **kwargs):
     service = kwargs.pop('service', None)
+    memoize = kwargs.pop('memoize', True)
     if not service:
         raise ServiceDoesNotExistException("Service not defined")
-    if service not in __service_configs:
+    if not memoize or service not in __service_configs:
         __service_configs[service] = ConfFile(*args, **kwargs)
     return getattr(__service_configs[service], service)
