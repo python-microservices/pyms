2ca96526361f2520710cb4ff420faf4d93f054f8
avara1986 <a.vara.1986@gmail.com>
2020-02-15 13:04:38 +0100

    Fix flake8

--- pyms/exceptions.py
+++ pyms/exceptions.py
@@ -16,8 +16,10 @@ class ServiceDoesNotExistException(Exception):
 class ConfigDoesNotFoundException(Exception):
     pass
 
+
 class ConfigErrorException(Exception):
     pass
+
 
 class PackageNotExists(Exception):
     pass
