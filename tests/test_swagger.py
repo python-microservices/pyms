13412d4c7d7a92fad3f66e42de59e80c8ab6b3c5
Àlex Pérez <alexperezpujol@disroot.org>
2020-03-13 19:42:17 +0100

    Merge pull request #96 from python-microservices/fix/connexion_path
    Fix connexion issue #1135

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -21,5 +21,31 @@ class SwaggerTests(unittest.TestCase):
         self.assertEqual("Python Microservice Swagger", self.app.config["APP_NAME"])
 
     def test_default(self):
-        response = self.client.get('/ws-doc/')
+        response = self.client.get('/test-api-path/ws-doc/')
+        self.assertEqual(200, response.status_code)
+
+    def test_home(self):
+        response = self.client.get('/test-api-path/')
+        self.assertEqual(200, response.status_code)
+
+
+class SwaggerNoAbsPathTests(unittest.TestCase):
+    """Test common rest operations wrapper.
+    """
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    def setUp(self):
+        os.environ[CONFIGMAP_FILE_ENVIRONMENT] = os.path.join(self.BASE_DIR, "config-tests-swagger_no_abs_path.yml")
+        ms = MyMicroserviceNoSingleton(path=__file__)
+        self.app = ms.create_app()
+        self.client = self.app.test_client()
+        self.assertEqual("Python Microservice Swagger2", self.app.config["APP_NAME"])
+
+    def test_default(self):
+        response = self.client.get('/test-api-path2/ws-doc2/')
+        self.assertEqual(200, response.status_code)
+
+    def test_home(self):
+        response = self.client.get('/test-api-path2/no-abs-path')
         self.assertEqual(200, response.status_code)
