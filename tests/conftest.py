bbc1cde783642e3c30f2624f7c3ede35596f3eab
avara1986 <a.vara.1986@gmail.com>
2020-11-09 18:44:51 +0100

    fix: wait until docker is up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,30 @@
 import os
 
 import pytest
+import requests
+from requests.exceptions import ConnectionError
 
 os.environ["ASYNC_TEST_TIMEOUT"] = os.environ.get("ASYNC_TEST_TIMEOUT", "15")
+
+
+def is_responsive(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+    except ConnectionError:
+        return False
+
+
+@pytest.fixture(scope="session")
+def http_service(docker_ip, docker_services):
+    """Ensure that HTTP service is up and responsive."""
+    url = "http://{}:{}".format(docker_ip, 8500)
+
+    docker_services.wait_until_responsive(
+        timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
+    )
+    return docker_ip
 
 
 @pytest.fixture(scope="session")
@@ -12,7 +34,7 @@ def docker_compose_file(pytestconfig):
 
 
 @pytest.fixture(scope="session")
-def config_env(docker_ip, docker_services):
+def config_env(http_service):
     """Set config for docker"""
-    os.environ["CONSUL_HOST"] = docker_ip
+    os.environ["CONSUL_HOST"] = http_service
     os.environ["CONSUL_PORT"] = "8500"
