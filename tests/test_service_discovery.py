5ebfc242f70c802750cc359e637cc964eb291c01
avara1986 <a.vara.1986@gmail.com>
2020-11-18 19:29:36 +0100

    tests: updated service discovery

--- tests/test_service_discovery.py
+++ tests/test_service_discovery.py
@@ -2,12 +2,44 @@
 """
 import os
 import unittest
-
-import requests_mock
+from unittest.mock import patch
 
 from pyms.constants import CONFIGMAP_FILE_ENVIRONMENT
 from pyms.flask.app import Microservice
-from pyms.flask.services.service_discovery import ServiceDiscoveryConsul
+from pyms.flask.services.service_discovery import ServiceDiscoveryBase, ServiceDiscoveryConsul
+
+
+class MockServiceDiscovery(ServiceDiscoveryBase):
+    pass
+
+
+class ServiceDiscoveryConsulTests(unittest.TestCase):
+    """Test service discovery services"""
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    def setUp(self):
+        os.environ[CONFIGMAP_FILE_ENVIRONMENT] = os.path.join(
+            self.BASE_DIR, "config-tests-service-discovery-consul.yml"
+        )
+        ms = Microservice(path=__file__)
+        self.ms = ms
+
+    @patch.object(ServiceDiscoveryConsul, "register_service", return_value=None)
+    def test_init(self, mock_consul):
+        """ test if the initializacion of service discovery call to the Service Discovery server to autoregister"""
+        self.ms.create_app()
+
+        mock_consul.assert_called_once_with(
+            app_name="Python Microservice Service Discovery",
+            healtcheck_url="http://127.0.0.1.nip.io:5000/healthcheck",
+            interval="10s",
+        )
+
+    def test_get_client(self):
+        client = self.ms.service_discovery.get_client()
+
+        self.assertTrue(isinstance(client, ServiceDiscoveryConsul))
 
 
 class ServiceDiscoveryTests(unittest.TestCase):
@@ -18,19 +50,11 @@ class ServiceDiscoveryTests(unittest.TestCase):
     def setUp(self):
         os.environ[CONFIGMAP_FILE_ENVIRONMENT] = os.path.join(self.BASE_DIR, "config-tests-service-discovery.yml")
         ms = Microservice(path=__file__)
+        ms.reload_conf()
         self.ms = ms
 
-    @requests_mock.Mocker()
-    def test_init(self, mock_request):
-        """ test if the initializacion of service discovery call to the Service Discovery server to autoregister"""
-        url = "http://localhost:8500/v1/agent/check/register"
-
-        mock_request.put(url)
-        self.ms.create_app()
-
-        self.assertTrue(True)
-
     def test_get_client(self):
+        self.ms.create_app()
         client = self.ms.service_discovery.get_client()
 
-        self.assertTrue(isinstance(client, ServiceDiscoveryConsul))
+        self.assertTrue(isinstance(client, MockServiceDiscovery))
